33947e973013b3a7e28c87f4b69acf69734535eb
mjlovell100 <mjlovell100@gmail.com>
2025-08-06 22:25:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,10 @@
-/* Your main React app file - already written */
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Hypertrophy Trainer App</h1>
+      <p>This is your custom workout tracker. More features coming soon!</p>
+    </div>
+  );
+}
